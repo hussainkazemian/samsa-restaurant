288cbc144575096fe7b6b98343a18f7dbc2fdd90
hussainkazemian <hussain.kazemian@metropolia.fi>
2024-12-10 16:23:17 +0200

    playwright report

--- playwright.config.js
+++ playwright.config.js
@@ -72,7 +72,7 @@ module.exports = defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start',
-    url: 'http://127.0.0.1:000',
+    url: 'http://127.0.0.1:3000',
     timeout: 120 * 1000, // Wait for up to 120 seconds for the server to start
     reuseExistingServer: !process.env.CI,
   },
